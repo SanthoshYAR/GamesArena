adb807282498968373d8a8d88e3139ad12084561
patha <ajaykumar.pathuri@in.pega.com>
2020-05-13 12:18:58 +0530

    readme removed

---
+++ build/precache-manifest.1171605db17451de9df79119a162b17f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9872a5d5361edc83d62a6987bacf2285",
+    "url": "/GamesArena/index.html"
+  },
+  {
+    "revision": "a97ec8e9e903367c1162",
+    "url": "/GamesArena/static/css/main.b9781f1e.chunk.css"
+  },
+  {
+    "revision": "1efaa641aa39a1c9e65e",
+    "url": "/GamesArena/static/js/2.c284362f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/GamesArena/static/js/2.c284362f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97ec8e9e903367c1162",
+    "url": "/GamesArena/static/js/main.f5accdb7.chunk.js"
+  },
+  {
+    "revision": "e8457b6a7c31c60b1459",
+    "url": "/GamesArena/static/js/runtime-main.4db9aa2a.js"
+  },
+  {
+    "revision": "af5e8dd51a60eb903bb2faa5f6ae2788",
+    "url": "/GamesArena/static/media/editors-choice.af5e8dd5.jpg"
+  }
+]);
